16ec73334bb76f02819a88c8ff9dd56d0e51330f
Pavel Proyavin <pavel@dorohovdesign.com>
2019-10-15 12:41:52 +0300

    release

---
+++ README.md
@@ -0,0 +1,14 @@
+### В шаблоне есть модалки, вызываются они так:
+
+```javascript
+openModal(modalID);
+```
+
+#### Options
+
+- ModalID - Идентификатор модального окна
+
+#### IDs:
+
+- #modal_form - Форма
+- #modal_success - Успешная отправка формы
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,30 @@
 (function($) {
     "use strict"
     $(function() {
+
+        $('form').parsley()
+
+        var phoneInputs = document.getElementsByClassName('input-phone');
+
+        if(phoneInputs.length) {
+            for(var i = 0; i < phoneInputs.length; i++) {
+                new IMask(
+                    phoneInputs[i], {
+                    mask: '+{7}(900)000-00-00'
+                });
+            }
+        }
+
+        $('.anchor').on('click', function(e) {
+            e.preventDefault();
+            var _this = $(this)
+            var aid = _this.attr("href");
+            if(!aid) {
+                aid = _this.data('target')
+            }
+            $('html,body').animate({scrollTop: $(aid).offset().top},'slow');
+        })
+
         
         function setPaddings() {
 
@@ -34,16 +58,6 @@
             setPaddings()
         })
 
-        $('.anchor').on('click', function(e) {
-            e.preventDefault();
-            var _this = $(this)
-            var aid = _this.attr("href");
-            if(!aid) {
-                aid = _this.data('target')
-            }
-            $('html,body').animate({scrollTop: $(aid).offset().top - $('.navbar').innerHeight()},'slow');
-        })
-
         $('.sreviews__list__inner').slick({
             slidesToShow: 3,
             slidesToScroll: 3,
@@ -69,24 +83,6 @@
             ]
         })
 
-        $('form').parsley()
-
-        var phoneInputs = document.getElementById('in-phone');
-
-        new IMask(
-            phoneInputs, {
-            mask: '+{7}(900)000-00-00'
-        });
-
-        $('.anchor').on('click', function(e) {
-            e.preventDefault();
-            var _this = $(this)
-            var aid = _this.attr("href");
-            if(!aid) {
-                aid = _this.data('target')
-            }
-            $('html,body').animate({scrollTop: $(aid).offset().top - $('.navbar').innerHeight()},'slow');
-        })
         
     })
 })(jQuery);
